7a1b9f0388c3135be5ec5ecfcb772d2e85807d5c
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-05-05 16:12:38 -0300

    feat: initial project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className={styles.container}>
+        <h1>Hello ToDo</h1>
 
-export default App
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam voluptatem quo totam. Velit hic ipsum nobis explicabo, dolor fugit numquam repellat voluptas quibusdam eveniet mollitia excepturi debitis esse aspernatur reiciendis?</p>
+        <p>Perferendis voluptatum, dicta molestias possimus nam libero, quibusdam harum enim sapiente culpa aspernatur eveniet repudiandae non nisi ullam quia? Voluptas aliquid nisi ullam quod ad dolor modi illum illo nihil?</p>
+        <p>Pariatur repellat ducimus esse natus harum voluptate dolore, aut consequatur impedit totam. Officiis repellat est itaque porro impedit. Ratione aut provident neque fuga alias iste, ipsa fugiat in a necessitatibus.</p>
+        <p>Vitae, perferendis et quod, consequatur debitis ducimus voluptatum id cum culpa consectetur quo quos, alias praesentium eius doloribus atque soluta corrupti modi quia laborum sit ullam? Voluptatum rerum neque nesciunt.</p>
+        <p>Maiores ipsam fugit aliquam incidunt adipisci ullam aperiam alias, excepturi commodi iusto quisquam sint blanditiis voluptates odit dolorum libero fuga quasi tempore reprehenderit distinctio eius dolorem consectetur eaque? Repellat, ullam?</p>
+      </div>
+    </>
+  );
+}
